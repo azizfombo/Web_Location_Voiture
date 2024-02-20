d66f6e883e5b72270e064c9a1ba10c642c18dff8
Emmanuel Pondi <nzouawae@3il.fr>
2024-02-20 10:28:33 +0100

    design1

--- View/login.php
+++ View/login.php
@@ -40,20 +40,64 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 ?>
 
 
-<div class="container mt-5">
-    <form method="POST">
-        <div class="form-group">
-            <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
-            <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+<section class="vh-100">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-4 text-black" style="background-color:#CD7F32">
+
+        <div class="px-5 ms-xl-4">
+          <i class="fas fa-car fa-4x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+          <span class="h1 fw-bold mb-0">Rent & Drive</span>
         </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+
+        <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+          <form method ="POST" style="width: 23rem;">
+
+            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+            <div class="form-outline mb-4">
+              <input type="email" id="email" name="email" class="form-control form-control-lg" placeholder="xxxxx@hotmail.fr" required>
+              <label class="form-label" for="email">Email address</label>
+            </div>
+
+            <div class="form-outline mb-4">
+              <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="******" required>
+              <label class="form-label" for="password">Password</label>
+            </div>
+
+            <div class="pt-1 mb-4">
+              <button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+            </div>
+
+            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Mot de passe oublié ?</a></p>
+
+          </form>
+
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+
+      </div>
+      <div class="col-sm-8 px-0 d-none d-sm-block">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="images/voiture5.webp" class="d-block w-100 vh-100" alt="Login image" style="object-fit: cover; object-position: left;">
+            </div>
+            <div class="carousel-item">
+              <img src="images/voiture6.webp" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="images/voiture7.webp" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="images/voiture8.webp" class="d-block w-100" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 <?php require 'inc/footer.php' ?>
